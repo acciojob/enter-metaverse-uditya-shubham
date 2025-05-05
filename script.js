feb093cfd159cc1f36019584bf9f36ce8184f990
Uditya Kumar <shubhamkumaar2001@gmail.com>
2025-05-05 23:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const statusPara = document.getElementById("status");
+    const enterBtn = document.getElementById("enterBtn");
+
+    enterBtn.addEventListener("click", function () {
+      // Create new h1 element
+      const newHeading = document.createElement("h1");
+      newHeading.textContent = "Entered Metaverse";
+
+      // Replace the <p> with the <h1>
+      statusPara.replaceWith(newHeading);
+    });
