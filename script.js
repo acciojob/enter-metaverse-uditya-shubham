cc9710216da30a4f1bc4059a61884dbc0cb865d2
Uditya Kumar <shubhamkumaar2001@gmail.com>
2025-05-05 23:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 //your JS code here. If required.
-const statusPara = document.getElementById("status");
+const statusDiv = document.getElementById("status");
     const enterBtn = document.getElementById("enterBtn");
 
     enterBtn.addEventListener("click", function () {
-      // Create new h1 element
-      const newHeading = document.createElement("h1");
-      newHeading.textContent = "Entered Metaverse";
-		newHeading.id = "status";
-
-      // Replace the <p> with the <h1>
-      statusPara.replaceWith(newHeading);
+      statusDiv.innerHTML = "<h1>Entered Metaverse</h1>"; // ✅ insert <h1> inside a div#status
     });
